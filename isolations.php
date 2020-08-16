1ef166e84fc9b36969cf2db88ea8bae095095d54
Paul Osman <paul@biocoda.com>
2020-08-16 23:22:52 +0100

    cards fully working

--- isolations.php
+++ isolations.php
@@ -3,16 +3,14 @@
     include("sessionCheck.php");
 ?>
 
-
-    <!-- CARD DECK HTML-->	
-    <div class="mainContent">
-        <div class="jumbotron isocd-jtron">
-            <p class="page-title">Current Site Isolations</p>
-            <hr class="isolTitleHR">
-            <!-- CARD ROW (3) -->
-            <div class="card-deck">
+<!-- CARD DECK HTML-->	
+<div class="mainContent">
+    <div class="jumbotron isocd-jtron">
+        <p class="page-title">All Current Site Isolations</p>
+        <hr class="isolTitleHR">
+        <!-- CARD ROW (3) -->
+        <div class="card-deck">
 <?php
-
 
     include("db_connection.php");
 
@@ -23,139 +21,124 @@
         while ($row = mysqli_fetch_assoc($result)) {
 
             ?>
-                        <!-- CARD HTML -->
-                        <div class="card isocd-card">
-                            <div class="card-header isocd-card-header">
-                                <ul class="nav nav-tabs card-header-tabs" role="tablist">
-                                    <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#isocd1-iso">Isolation</a></li>
-                                    <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#isocd1-scs">Sources Isolated</a></li>
-                                </ul>
-                            </div>
-                            <div class="tab-content">	
-                                <div class="tab-pane active" id="isocd1-iso">
-                                    <div class="card-body isocd-card-body">
-                                        <h5 class="card-title isocd-card-title"><?php echo $row['asset_name'];?></h5>
-                                        <p class="card-text isocd-card-text"><?php echo $row['description'];?></p>
-                                        <hr class="isocd-divider"> 
-                                        <ul class="isocd-ul">
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa far fa-calendar-check"></i></div>
-                                                    <div class="col-8 isocd-value"><p class="isocd-value-p"><?php echo $row['date_isolated'];?></p></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa far fa-user"></i></div>
-                                                    <div class="col-8 isocd-value"><p class="isocd-value-p"><?php echo $row['user_firstName']."&nbsp".$row['user_lastName'];?></p></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fa fa-phone-alt"></i></div>
-                                                    <div class="col-8 isocd-value"><p class="isocd-value-p"><?php echo $row['user_telephone'];?></p></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <!-- <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa far fa-file-alt"></i></div>
-                                                    <div class="col-8 isocd-value"><p class="isocd-value-p">13223</p></div>
-                                                </div>
-                                            </div>
-                                        </li> -->
-                                        </ul>
-                                        <hr class="isocd-divider"> 
-                                        <a href="#" class="btn btn-primary isocd-btn"><i class="fas fa-unlock-alt"></i>&nbsp;&nbsp;&nbsp;&nbsp;Remove Isolation</a>
-                                    </div>
+
+            <div class="card isoCard-card">
+                <div class="card-header">
+                    <h5 class="card-title"><?php echo $row['asset_name'];?></h5>
+                    <p class="card-text"><?php echo $row['description'];?></p>
+                </div>
+                <div class="card-body">
+                    <ul class="isocd-ul">
+                        <li class="isocd-li">
+                            <div class="container">
+                                <div class="row ">
+                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa far fa-calendar-check"></i></div>
+                                    <div class="col-8 isocd-value"><p class="isocd-value-p"><?php echo $row['date_isolated'];?></p></div>
                                 </div>
-                                <!-- SOURCES TAB -->
-                                <div class="tab-pane" id="isocd1-scs">
-                                    <div class="card-body isocd-card-body">
-                                        <ul class="isocd-ul">
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-fan"></i></div>
-                                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Compressed Air</p></div>
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa isocd-tickMark fas fa-lock"></i></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-water"></i></div>
-                                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Water</p></div>
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa isocd-tickMark fas fa-lock"></i></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-wind"></i></div>
-                                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Steam</p></div>
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa isocd-tickMark fas fa-lock"></i></div>
-                                                </div>
-                                            </div>
-                                        </li>			
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-bolt"></i></div>
-                                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Electricity</p></div>
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa-ltgrey fas fa-lock-open"></i></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-snowflake"></i></div>
-                                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Ethylene Glycol (CH<sub>2</sub>H<sub>6</sub>O<sub>2</sub>)</p></div>
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa isocd-tickMark fas fa-lock"></i></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-flask"></i></div>
-                                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Ammonia (NH<sub>3</sub>)</p></div>
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa isocd-tickMark fas fa-lock"></i></div>
-                                                </div>
-                                            </div>
-                                        </li>			
-                                        <li class="isocd-li">
-                                            <div class="container">
-                                                <div class="row ">
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-flask"></i></div>
-                                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Peroxide (H<sub>2</sub>O<sub>2</sub>)</p></div>
-                                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa isocd-tickMark fas fa-lock"></i></div>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        </ul>
-                                    </div><!-- CARD BODY -->
-                                </div><!-- TAB PANE -->
-                            </div><!-- TAB CONTENT -->
-                        </div><!-- CARD 1 -->
-                        
+                            </div>
+                        </li>
+                        <li class="isocd-li">
+                            <div class="container">
+                                <div class="row ">
+                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa far fa-user"></i></div>
+                                    <div class="col-8 isocd-value"><p class="isocd-value-p"><?php echo $row['user_firstName']."&nbsp".$row['user_lastName'];?></p></div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="isocd-li">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fa fa-phone-alt"></i></div>
+                                    <div class="col-8 isocd-value"><p class="isocd-value-p"><?php echo $row['user_telephone'];?></p></div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                <hr class="isocd-divider"> 
+                    <ul class="isocd-ul">
+                        <li class="isocd-li">
+                            <div class="container">
+                                <div class="row ">
+                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-wind"></i></div>
+                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Steam</p></div>
+                                    <div class="col-1 isocd-fa-col1"><i id="steam<?php echo $row['date_isolated'];?>" class="isocd-grey fas fa-lock-open"></i></div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="isocd-li">
+                            <div class="container">
+                                <div class="row ">
+                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-water"></i></div>
+                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Water</p></div>
+                                    <div class="col-1 isocd-fa-col1"><i id="water<?php echo $row['date_isolated'];?>" class="isocd-grey fas fa-lock-open"></i></div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="isocd-li">
+                            <div class="container">
+                                <div class="row ">
+                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-fan"></i></div>
+                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Compressed Air</p></div>
+                                    <div class="col-1 isocd-fa-col1"><i id="air<?php echo $row['date_isolated'];?>" class="isocd-grey fas fa-lock-open"></i></div>
+                                </div>
+                            </div>
+                        </li>			
+                        <li class="isocd-li">
+                            <div class="container">
+                                <div class="row ">
+                                    <div class="col-1 isocd-fa-col1"><i class="isocd-fa fas fa-bolt"></i></div>
+                                    <div class="col-10 isocd-value"><p class="isocd-value-p">Electricity</p></div>
+                                    <div class="col-1 isocd-fa-col1"><i id="elec<?php echo $row['date_isolated'];?>" class="isocd-grey fas fa-lock-open"></i></div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                <hr class="isocd-divider"> 
+                    <a href="#" class="btn btn-primary isocd-btn"><i class="fa fa-unlock-alt"></i>&nbsp;&nbsp;&nbsp;&nbsp;Remove Isolation</a> 
+                </div>
+            </div>
+                     
+            <script type="text/javascript">
+
+                if (<?php echo $row['comp_air_isolated'];?> == 1) {
+
+                    var caIcon = document.getElementById("air<?php echo $row['date_isolated'];?>");
+                    caIcon.className = caIcon.className.replace("fa-lock-open", " fa-lock");
+                    caIcon.className = caIcon.className.replace("isocd-grey", " isocd-red");
+                }
+
+                if (<?php echo $row['water_isolated'];?> == 1) {
+
+                    var caIcon = document.getElementById("water<?php echo $row['date_isolated'];?>");
+                    caIcon.className = caIcon.className.replace("fa-lock-open", " fa-lock");
+                    caIcon.className = caIcon.className.replace("isocd-grey", " isocd-red");
+                }
+
+                if (<?php echo $row['steam_isolated'];?> == 1) {
+
+                    var caIcon = document.getElementById("steam<?php echo $row['date_isolated'];?>");
+                    caIcon.className = caIcon.className.replace("fa-lock-open", " fa-lock");
+                    caIcon.className = caIcon.className.replace("isocd-grey", " isocd-red");
+                }
+
+                if (<?php echo $row['electricity_isolated'];?> == 1) {
+
+                    var caIcon = document.getElementById("elec<?php echo $row['date_isolated'];?>");
+                    caIcon.className = caIcon.className.replace("fa-lock-open", " fa-lock");
+                    caIcon.className = caIcon.className.replace("isocd-grey", " isocd-red");
+                }
+
+            </script>
 
          <?php    
         }
+    } else {
+        echo "There are no registered isolations";
     }
 ?>
-                    </div><!-- CARD DECK --> 	
-                </div><!-- F1 JUMBOTRON -->	
-            </div><!-- F1 MAIN CONTENT -->
+        </div><!-- CARD DECK --> 	
+    </div><!-- F1 JUMBOTRON -->	
+</div><!-- F1 MAIN CONTENT -->
 
 <?php include("footer.php"); ?>
 
--- sessionCheck.php
+++ sessionCheck.php
@@ -10,7 +10,7 @@ if (array_key_exists("id", $_COOKIE)) {
 
 if (array_key_exists("id", $_SESSION)) {
 
-    echo "<a href='index.php?logout=1' style='position: fixed; z-index: 999; width:52px; height:52px; bottom:22px; right:24px; background-color:#C36000; color:#FFF; border-radius:50px; text-align:center; box-shadow: 2px 2px 3px #999;'><i class='fas fa-sign-out-alt fa-2x' style='margin-top: 12px;'></i></a>";
+    echo "<a href='index.php?logout=1' style='position: fixed; z-index: 999; width:52px; height:52px; bottom:22px; right:24px; background-color:#C36000; color:#FFF; border-radius:50px; text-align:center; box-shadow: 2px 2px 3px #999;'><i class='fas fa-sign-out-alt fa-2x' style='margin-top:13px; margin-left:2px;'></i></a>";
 
 } else {
 
